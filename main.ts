904acea759d9b4f2693a6281ccf58bc95c41c069
Waqar Ahmed <wa940150@gmail.com>
2024-04-25 12:00:35 +0500

    My first commit

---
+++ main.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer";
+
+const answers : {
+    sentence: string
+} = await inquirer.prompt([
+    {
+      name: "sentence",
+      type: "input",
+      message: "Enter your sentence to count the word:"
+    }
+]);
+
+
+const words = answers.sentence.trim().split(" ")
+
+// print the array of word to the console
+console.log(words)
+
+// print the word count of the sentence to the console
+console.log(`Your sentence word count is ${words.length}`);
